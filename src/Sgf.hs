5b552db531203519dee9941d86fcf44a0f85623c
ron <rinbar70@gmail.com>
2023-08-03 17:21:09 +0300

    23 examples, 2 failures

--- src/Sgf.hs
+++ src/Sgf.hs
@@ -2,11 +2,18 @@ module Sgf
   ( parseSgf
   ) where
 
-import           Control.Monad.State (evalStateT)
-import           Data.Map            (Map)
-import           Data.Text           (Text)
-import           Data.Tree           (Tree)
-import           Sgf.Internal        (parser)
+import           Control.Applicative  (Alternative (..))
+import           Data.Char            (isUpper)
+import           Data.Map             (Map, fromList)
+import           Data.Maybe           (fromMaybe)
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import           Data.Tree            (Tree (..))
+import           Data.Void            (Void)
+import           Text.Megaparsec      (MonadParsec (..), Parsec, anySingle,
+                                       anySingleBut, between, manyTill,
+                                       runParser, satisfy)
+import           Text.Megaparsec.Char (char)
 
 -- | A tree of nodes.
 type SgfTree = Tree SgfNode
@@ -15,5 +22,35 @@ type SgfTree = Tree SgfNode
 -- Keys may have multiple values associated with them.
 type SgfNode = Map Text [Text]
 
+type Parser = Parsec Void Text
+
+propName :: Parser Text
+propName = T.pack <$> some (satisfy isUpper)
+
+propValue :: Parser Text
+propValue = T.pack <$ char '[' <*> manyTill charLiteral (char ']')
+  where
+    charLiteral :: Parser Char
+    charLiteral =
+      convert [('\t', ' '), ('\n', ' ')] <$ char '\\' <*> anySingle <|>
+      convert [('\t', ' ')] <$> anySingleBut '\\'
+    convert t c = fromMaybe c (lookup c t)
+
+prop :: Parser (Text, [Text])
+prop = (,) <$> propName <*> some propValue
+
+node :: Parser SgfNode
+node = fromList <$ char ';' <*> many prop
+
+branch :: Parser SgfTree
+branch =
+  try ((\x y -> Node x [y]) <$> node <*> branch) <|> Node <$> node <*> many tree
+
+tree :: Parser SgfTree
+tree = between (char '(') (char ')') branch
+
 parseSgf :: String -> Maybe SgfTree
-parseSgf = evalStateT parser
+parseSgf s =
+  case runParser (tree <* eof) "" (T.pack s) of
+    Left _  -> Nothing
+    Right t -> Just t
